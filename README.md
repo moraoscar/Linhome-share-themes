3e7ee0bccd1e783b54737a2e9fd404981e6c46bb
Christophe Deschamps <christophe.deschamps.work@gmail.com>
2020-06-24 16:38:23 +0200

    Inital version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # lindoor-shared-themes
 
+Added as submodule of lindoor-ios and lindoor-android app, contains the shared resources (theme, texts, etc).
+
+Used at build time by Android and iOS Apps
+
---
+++ zip.sh
@@ -0,0 +1,4 @@
+zip -r lindoor.zip action_types.xml method_types.xml device_types.xml texts.xml theme.xml images fonts  
+cp lindoor.zip ../lindoor-android/app/src/main/assets/lindoor.zip
+cp lindoor.zip ../lindoor-ios/Resources/lindoor.zip
+
